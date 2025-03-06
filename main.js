2c5b54cbfa4531d9c97cfe361fcb66b2e4828b9f
bwyoungy <youngyaacov@gmail.com>
2025-03-06 17:28:35 +0200

    add validation that all cells are filled and allow only uppercase letters as input

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// Iterate over all input cells and allow to input only capital letters
+document.querySelectorAll('.input-cell').forEach(cell => {
+    cell.addEventListener('input', function() {
+        // Replace lowercase letters with uppercase letters and replace anything which isn't a letter with an empty space
+        this.value = this.value.toUpperCase().replace(/[^A-Z]/g, '');
+    });
+});
+
+// Function to validate input. Returns boolean value if validation passes or not
+function validateGridInput() {
+    const cells = document.querySelectorAll('.input-cell');
+    for (let cell of cells) {
+        if (cell.value.trim() === '') {
+            alert("Please fill in all letter cells to solve!");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Algorithm to solve Waffle puzzle
 function solveWaffle() {
-    
+    // Validate input and stop function if invalid
+    if (!validateGridInput()) return;
 }
